d12188f3c6ceca6fc727a085065d77aaa7fab304
CaCtUs1k <denischernish19012004@gmail.com>
2023-12-11 15:37:43 +0200

    HTML architecture update

--- main/urls.py
+++ main/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from main.views import view_comments, add_comment
 
+app_name = "main"
+
 urlpatterns = [
     path('', view_comments, name='view_comments'),
     path('add_comment/', add_comment, name='add_comment'),
