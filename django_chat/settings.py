1df524ac1a50496828a6dca06f205daccd0122cb
CaCtUs1k <denischernish19012004@gmail.com>
2023-12-11 15:38:39 +0200

    prepare register feature

--- django_chat/settings.py
+++ django_chat/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "crispy_forms",
+    "crispy_bootstrap4",
     'main.apps.MainConfig',
     'user',
 ]
@@ -126,3 +128,5 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'user.User'
+
+CRISPY_TEMPLATE_PACK = "bootstrap4"
