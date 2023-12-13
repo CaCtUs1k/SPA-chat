a8806f5de8a59a8a5d7a458b4e9df74feb0ede06
CaCtUs1k <denischernish19012004@gmail.com>
2023-12-13 22:17:33 +0200

    install channels

--- django_chat/settings.py
+++ django_chat/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'channels',
     "crispy_forms",
     "crispy_bootstrap4",
     "debug_toolbar",
