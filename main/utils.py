42e7c56633ba66e9379661083643e1d8c4fc1c00
CaCtUs1k <denischernish19012004@gmail.com>
2023-12-14 19:23:23 +0200

    split add comment method

--- main/utils.py
+++ main/utils.py
@@ -1,8 +1,11 @@
+from io import BytesIO
+
+from PIL import Image
 from django.core.exceptions import ValidationError
 
 
 def validate_allowed_file_extensions(value):
-    allowed_extensions = ['.png', '.gif', '.jpg', '.jpeg', '.txt']
+    allowed_extensions = ['png', 'gif', 'jpg', 'jpeg', 'txt']
     if not any(value.name.lower().endswith(ext) for ext in allowed_extensions):
         raise ValidationError('Invalid file format. Allowed formats are .png, .gif, .jpg, .jpeg, .txt.')
 
@@ -17,3 +20,18 @@ def validate_file_field(value):
             raise ValidationError('Text file size exceeds 100 kB.')
     else:
         pass
+
+
+def compress_image(image):
+    valid_extensions = ['png', 'gif', 'jpg', 'jpeg']
+    extension = image.name.lower().split('.')[-1]
+    if extension in valid_extensions:
+        img = Image.open(image)
+        max_width = 320
+        max_height = 240
+        img.thumbnail((max_width, max_height))
+        image_io = BytesIO()
+        img.save(image_io, format='JPEG')
+
+        image_io.seek(0)
+        image.file = image_io
