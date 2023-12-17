6b6521082ce9e012fcefb715c3da0160dbddd9c9
CaCtUs1k <denischernish19012004@gmail.com>
2023-12-17 19:42:39 +0200

    clean up code

--- main/models.py
+++ main/models.py
@@ -8,14 +8,21 @@ class Comment(models.Model):
     sender = models.ForeignKey(User, on_delete=models.CASCADE)
     home_page = models.URLField(blank=True, null=True)
     text = models.TextField()
-    parent_comment = models.ForeignKey('self', null=True, blank=True, on_delete=models.CASCADE)
+    parent_comment = models.ForeignKey(
+        "self", null=True, blank=True, on_delete=models.CASCADE
+    )
     created_at = models.DateTimeField(auto_now_add=True)
 
-    attachment = models.FileField(upload_to='comment_attachments/', null=True, blank=True, max_length=255,
-                                  validators=[validate_file_field])
+    attachment = models.FileField(
+        upload_to="comment_attachments/",
+        null=True,
+        blank=True,
+        max_length=255,
+        validators=[validate_file_field],
+    )
 
     def __str__(self):
-        return f'{self.sender.username} {self.created_at}'
+        return f"{self.sender.username} {self.created_at}"
 
     def get_child_comments(self):
         return Comment.objects.filter(parent_comment=self)
--- main/urls.py
+++ main/urls.py
@@ -6,6 +6,6 @@ from main.views import add_comment, ViewComments
 app_name = "main"
 
 urlpatterns = [
-    path('', ViewComments.as_view(), name='view_comments'),
-    path('add_comment/', check_recaptcha(add_comment), name='add_comment'),
+    path("", ViewComments.as_view(), name="view_comments"),
+    path("add_comment/", check_recaptcha(add_comment), name="add_comment"),
 ]
