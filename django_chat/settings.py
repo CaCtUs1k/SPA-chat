4e4ea63296fc04150e1cc927f71efa33a37f14a2
CaCtUs1k <denischernish19012004@gmail.com>
2023-12-19 09:24:25 +0200

    update README.md

--- django_chat/settings.py
+++ django_chat/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "default")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DJANGO_DEBUG_MODE", "False")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
